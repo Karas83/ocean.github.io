6675019fef88a82252f2d8c44e241d0841792746
Rodion <sharoshkin1996@gmail.com>
2022-07-25 16:41:26 +0300

    swfix

--- js/script.js
+++ js/script.js
@@ -1,22 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const btn1 = document.querySelector('#btn1');
-    const modal1 = new bootstrap.Modal(document.querySelector('#myModal1'));
-    btn1.addEventListener('click', function () {
-        modal1.show();
-    });
-    const btn2 = document.querySelector('#btn2');
-    const modal2 = new bootstrap.Modal(document.querySelector('#myModal2'));
-    btn2.addEventListener('click', function () {
-        modal2.show();
-    });
-    const btn3 = document.querySelector('#btn3');
-    const modal3 = new bootstrap.Modal(document.querySelector('#myModal3'));
-    btn3.addEventListener('click', function () {
-        modal3.show();
-    });
-    $("#myModal1, #myModal2, #myModal3").on('hidden.bs.modal', function (e) {
-        alert("Спасибо, ожидайте звонок оператора!!!");
-    });
-
-});
-
+var swiper = new Swiper(".mySwipertopnft", {
+    slidesPerView: 3,
+    spaceBetween: 0,
+    allowTouchMove: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 35
+      },
+      860: {
+        slidesPerView: 2,
+        spaceBetween: 35
+      },
+      1300: {
+        slidesPerView: 3,
+        spaceBetween: 35
+      }
+    }
+  });
